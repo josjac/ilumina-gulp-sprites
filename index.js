0e38e106a7583d6195e77b5531e9263acfa7f240
jayon <josalfredyon@gmail.com>
2015-11-24 17:14:45 -0500

    Cambio uso y configuracion

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
- * configuracion:
- *[
- *  {
- *    src: 'src/static/sprites',
- *    dest_img: 'dist/static/sprites',
- *    dest_css: 'src/static/sprites'
- *  }
- *]
-*/
-
 var gulp = require('gulp');
 
 var sprites = require('gulp.spritesmith');
@@ -17,47 +6,58 @@ var path = require('path');
 
 var fs = require('fs');
 
+var _ = require('lodash');
+
 var cwd = process.cwd();
 
 var default_config = {
   src: path.join(cwd, 'src', 'static', 'sprites'),
   dest_img: path.join(cwd, 'dist', 'static', 'sprites'),
-  dest_css: path.join(cwd, 'src', 'static', 'sprites')
+  dest_css: path.join(cwd, 'src', 'static', 'sprites'),
+  img_path: '../sprites/'
+};
+
+var self = {
+  config: default_config,
+  run: function(config) {
+    config = _.assign(this.config, config);
+    genSprites(config);
+  }
 };
 
 function getFolders(dir) {
   return fs.readdirSync(dir)
-    .filter(function(file) {
-      return fs.statSync(path.join(dir, file)).isDirectory();
-    });
+
+  .filter(function(file) {
+    return fs.statSync(path.join(dir, file)).isDirectory();
+  });
 }
 
-module.exports = function(configs) {
-  configs = configs || [default_config];
+function genSprites(config) {
+  getFolders(config.src).map(function(dir) {
+    var d = gulp.src(path.join(config.src, dir, '*.png'))
+      .pipe(sprites({
+        imgName: dir + '.png',
+        cssName: dir + '.styl',
+        imgPath: config.img_path + dir + '.png'
+      }));
 
-  gulp.task('sprites', function() {
+    if (config.dest) {
+      d.pipe(gulp.dest(config.dest));
+    }
 
-    configs.forEach(function(config) {
-      getFolders(config.src).map(function(dir) {
-        var d = gulp.src(path.join(config.src, dir, '*.png'))
-          .pipe(sprites({
-            imgName: dir + '.png',
-            cssName: dir + '.styl',
-            cssPath: '../sprites/'
-          }));
+    else {
+      d.img
+      .pipe(gulp.dest(config.dest_img));
 
-        if (config.dest) {
-          d.pipe(gulp.dest(config.dest));
-        }
-
-        else {
-          d.img
-          .pipe(gulp.dest(config.dest_img));
-
-          d.css
-          .pipe(gulp.dest(config.dest_css));
-        }
-      });
-    });
+      d.css
+      .pipe(gulp.dest(config.dest_css));
+    }
   });
-};
+}
+
+gulp.task('sprites', function() {
+  self.run();
+});
+
+module.exports = self;
